6908f556e8ec7f37a0b8888b5d7e2d34a65549f2
nmlakra <nikhilmarklakra@gmail.com>
2024-03-06 03:05:39 +0530

    refactored read_file and get_word_count

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-def main():
-    with open("books/frankenstein.txt", "r") as f:
-        file_contents = f.read()
+def read_file(path):
+    with open(path, "r") as f:
+        return f.read()
 
-    print(
-        len(file_contents.split())
-    )     
+
+def get_word_count(corpus):
+    return len(corpus.split())
+
+
+def main():
+    file_contents = read_file("books/frankenstein.txt")
+    print(get_word_count(file_contents))
+
 
 if __name__ == "__main__":
     main()
